bd239c01c63c405b199bb0954d95ce87de5cb473
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-05-28 13:07:25 +0200

    DD-66 user patch method test v5

--- src/store/userProfile.js
+++ src/store/userProfile.js
@@ -14,9 +14,9 @@ const INITIAL_STATE = {
 
 export const updateProfile = createAsyncThunk(
   "userProfile/updateProfile",
-  async ({ id, profile }) => {
+  async (profile) => {
     try {
-      const { data } = await api.updateUserProfile(id, profile);
+      const { data } = await api.updateUserProfile(profile);
       console.log("Thunk response after update");
       console.log(data);
       return data;
